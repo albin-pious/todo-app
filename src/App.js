ff1f88f5e5e126b9bb20023ccb62266013e4aade
Albin Pious <albinpious65@gmail.com>
2024-01-03 09:50:24 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const d = new Date();
+  let day = weekday[d.getDay()];
+  const [toDos,setToDos] = useState([]);
+  const [toDo,setToDo] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {day} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input type="text" placeholder="🖊️ Add item..." value={toDo} onChange={(e)=>setToDo(e.target.value)} />
+        <i className="fas fa-plus" onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])}></i>
+      </div>
+      <div className="todos">
+        { toDos.map((obj  )=>{
+          return ( 
+            <div className="todo">
+              <div className="left">
+              <input
+                type="checkbox"
+                checked={obj.status}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    const updatedToDos = toDos.filter((obj2) => obj2.id !== obj.id);
+                    setToDos(updatedToDos);
+                  } else {
+                    const updatedToDos = toDos.map((obj2) =>
+                      obj2.id === obj.id ? { ...obj2, status: e.target.checked } : obj2
+                    );
+                    setToDos(updatedToDos);
+                  }
+                }}
+              />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times" onClick={()=>{
+                   setToDos(prevToDos => prevToDos.filter(item => item !== obj));
+                }}></i>
+              </div>
+            </div> 
+          )
+        }) }
+      </div>
     </div>
   );
 }
